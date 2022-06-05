ea399e3b775703f51cdbe730d97dd123155d8dcc
vellone-fabricio <fabriciovellone@gmail.com>
2022-06-05 17:34:12 -0300

    [fv-36] Use generic make-response

--- src/project-price/project-price.controller.ts
+++ src/project-price/project-price.controller.ts
@@ -3,7 +3,7 @@ import { ProjectPriceService } from './project-price.service';
 import { GrpcMethod } from '@nestjs/microservices';
 import * as PROTO from '../common/proto-dto/financial-service/financial-service.pb';
 import { CreateProjectPriceDTO, FindByIdDTO, UpdateProjectPriceDTO } from '../project-price/dto/project-price.dto';
-import { makeResponseProjectPrice } from 'src/common/utils';
+import { makeResponse } from 'src/common/helpers/make-response';
 
 @Controller()
 export class ProjectPriceController {
@@ -14,34 +14,38 @@ export class ProjectPriceController {
   async create(createRequestBody: CreateProjectPriceDTO): Promise<PROTO.WithObjectResponse> {
     const createdObject = await this.projectPriceService.create(createRequestBody);
 
-    return makeResponseProjectPrice(createdObject, HttpStatus.CREATED);
+    return makeResponse(createdObject, {
+      httpStatus: HttpStatus.CREATED,
+    });
   }
 
   @GrpcMethod(PROTO.PROJECT_PRICE_SERVICE_NAME, 'find')
   async findAll(): Promise<PROTO.FindResponse> {
     const allProjectPrices = await this.projectPriceService.findAll();
 
-    return makeResponseProjectPrice(allProjectPrices);
+    return makeResponse(allProjectPrices);
   }
 
   @GrpcMethod(PROTO.PROJECT_PRICE_SERVICE_NAME, 'findOne')
   async findOne({ id }: FindByIdDTO): Promise<PROTO.WithObjectResponse> {
     const requestedProjectPrice = await this.projectPriceService.findOne(id);
 
-    return makeResponseProjectPrice(requestedProjectPrice);
+    return makeResponse(requestedProjectPrice);
   }
 
   @GrpcMethod(PROTO.PROJECT_PRICE_SERVICE_NAME, 'update')
   async update(payload: UpdateProjectPriceDTO): Promise<PROTO.WithObjectResponse> {
     const updatedModel = await this.projectPriceService.update(payload);
 
-    return makeResponseProjectPrice(updatedModel);
+    return makeResponse(updatedModel);
   }
 
   @GrpcMethod(PROTO.PROJECT_PRICE_SERVICE_NAME, 'delete')
   async remove({ id }: FindByIdDTO): Promise<PROTO.WithObjectResponse> {
     await this.projectPriceService.remove(id);
 
-    return makeResponseProjectPrice(null, HttpStatus.NO_CONTENT);
+    return makeResponse(null, {
+      httpStatus: HttpStatus.NO_CONTENT,
+    });
   }
 }
